8117edc59b7e3766a7d69f94979b3bbf9bda32df
JaBO <admin@jaboweb.com>
2015-11-23 20:01:13 +0100

    add SPANISH localization

---
+++ localization/es.js
@@ -0,0 +1,21 @@
+/**
+ * bs_pagination simple localization - SPANISH
+ *
+ * DO NOT CHANGE this file, as it will be overwritten in next update.
+ * To use different values, write and use a similar structure js file.
+ *
+ */
+var rsc_bs_pag = {
+    go_to_page_title: 'Ir a página',
+    rows_per_page_title: 'Resultados por página',
+    current_page_label: 'Página',
+    current_page_abbr_label: 'p.',
+    total_pages_label: 'de',
+    total_pages_abbr_label: '/',
+    total_rows_label: 'de',
+    rows_info_records: 'resultados',
+    go_top_text: '&laquo;',
+    go_prev_text: '&larr;',
+    go_next_text: '&rarr;',
+    go_last_text: '&raquo;'
+};
